5959d67edb327dce6d12bd1d5b3c36d5286eae5e
mdmdr8 <krystle.pk@gmail.com>
2024-08-04 16:40:09 +0900

    euqals !

---
+++ src/main/java/com/example/test/lang/Rectangular.java
@@ -0,0 +1,11 @@
+package com.example.test.lang;
+
+public class Rectangular {
+    private int with;
+    private int height;
+
+    public Rectangular(int with, int height) {
+        this.with = with;
+        this.height = height;
+    }
+}
---
+++ src/main/java/com/example/test/lang/RectangularMain.java
@@ -0,0 +1,14 @@
+package com.example.test.lang;
+
+public class RectangularMain {
+    public static void main(String[] args) {
+        Rectangular rect1 = new Rectangular(100, 20);
+        Rectangular rect2 = new Rectangular(100, 20);
+
+        System.out.println( rect2);
+        System.out.println( rect1);
+        System.out.println( rect2 == rect1);
+        System.out.println( rect2.equals(rect1));
+
+    }
+}
